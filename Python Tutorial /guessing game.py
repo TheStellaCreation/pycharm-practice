a6c0c0ff0dbe5fa4c7cb28c42a3c342d0738e9d4
xin li <xlds2020@gmail.com>
2021-09-15 23:09:20 -0700

    modify guessgame file name

--- Python Tutorial /guessing game.py
+++ Python Tutorial /guessing game.py
@@ -1,17 +1,18 @@
-secret_word = "stella"
-guess_word = ""
-guess_count = 0
-guess_limit = 3
+secret_word = "Stella"
+guess = ""
+guess_count= 0
+guess_limit= 3
 out_of_guesses = False
 
-while guess_word != secret_word and not out_of_guesses :
+while guess != secret_word and not (out_of_guesses):
     if guess_count < guess_limit:
-        guess_word = input ("Enter guess: ")
+        guess = input("Enter guess: ")
         guess_count += 1
+        print(f"you only have {guess_limit - guess_count} left")
     else:
         out_of_guesses = True
 
 if out_of_guesses:
-    print ("out of guesses, YOU LOSS!")
+    print ("you are out of guesses, lost the game")
 else:
-    print ("You win!")
+    print("You win!")
